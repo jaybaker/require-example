54c78afb5d6b367e1e3142fb838b8ccff7fae632
Jay Baker <jay@jaybakerweb.com>
2016-02-02 19:13:52 -0600

    Updated versions and cleaned up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+	concat = require('gulp-concat'),
+	minifyCSS = require('gulp-minify-css'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename');
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    uglify = require('gulp-uglify'),
-    plumber = require('gulp-plumber');
+    size = require('gulp-size'),
+    buffer = require('vinyl-buffer');
 
 gulp.task('scripts', function() {
     return browserify('./js/main.js', {debug: true})
         .bundle()
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(size())
         .pipe(source('bundle.js'))
-        //gulp.src('static/js/*.js')
-        //.pipe(plumber)
-        //.pipe(uglify())
         .pipe(gulp.dest('build/js'));
 });
 
